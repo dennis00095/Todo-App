583a47bb5895576074d60ed08a009c127f668129
dennis00095 <mwongeladennis9@gmail.com>
2020-03-02 12:58:15 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-App';
+  title = 'My Todos';
+  todos = [ 
+    {
+    label: 'Bring Milk',
+    done: false,
+    priority: 3
+  },
+  {
+    label: 'Watch Movie',
+    done: false,
+    priority: 4
+  }, 
+  {
+    label: 'Clean House',
+    done: true,
+    priority: 1
+  },
+  {
+    label: 'Work Out',
+    done: false,
+    priority: 2
+  }
+];
+
+addTodo(newTodoLabel){
+  var newTodo = {
+    label: newTodoLabel,
+    priority: 1,
+    done: false
+
+  };
+  this.todos.push(newTodo);
+}
+deleteTodo(todo){
+  this.todos = this.todos.filter(t => t.label !==todo.label)
+}
 }
